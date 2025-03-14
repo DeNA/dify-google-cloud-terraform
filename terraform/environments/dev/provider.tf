3dd50021a7aaa1a16f69e9424706425d11b2f1b7
sota-a-oizumi <sota.a.oizumi@dena.com>
2025-03-15 01:02:27 +0900

    chore: use latest google provider

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -1,20 +1,4 @@
 terraform {
-  required_version = ">=1.5.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.10.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "5.10.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "2.4.0"
-    }
-  }
-
   backend "gcs" {
     bucket = "your-tfstate-bucket" # replace with your bucket name
     prefix = "dify"
